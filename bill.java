b09c92e2e8424c55f69264f40ec97a4805016c60
chaudharigaurav865@gmail.com <Gaurav@17092002>
2025-05-19 12:18:09 +0530

    Commited

---
+++ bill.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class bill {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a bill amount: ");
+        int bill_amt=sc.nextInt();
+        int total = bill_amt;
+        if(bill_amt<200){
+            total = bill_amt;
+        }else if(bill_amt >= 200 && bill_amt <= 500 ){
+            total = (int) (bill_amt -(bill_amt * 0.01));
+        }else if(bill_amt >= 500 && bill_amt<=1000){
+            total = (int) (bill_amt -(bill_amt* 0.05));
+        }
+        System.out.println(total);  
+    }
+}
