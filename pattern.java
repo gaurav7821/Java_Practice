1ebb09e4c89aacf2179a80a1651138a575dc69aa
chaudharigaurav865@gmail.com <chaudharigaurav865@gmail.com>
2025-05-23 19:55:09 +0530

    Pattern display

---
+++ pattern.java
@@ -0,0 +1,15 @@
+//WAP to display the pattern
+import java.util.*;
+
+public class pattern {
+    
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
