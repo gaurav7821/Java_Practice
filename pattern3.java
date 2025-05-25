89cd68ee3f7a950b62296860b3b4dcd341f788d7
chaudharigaurav865@gmail.com <chaudharigaurav865@gmail.com>
2025-05-25 13:03:29 +0530

    Pattern 3

---
+++ pattern3.java
@@ -0,0 +1,21 @@
+//WAP to pattern 3
+import java.util.*;
+
+public class pattern3 {
+    
+    public static void main(String[] args) {
+        
+        for(int i=1 ; i<=5 ; i++){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(" ");
+            }
+            for(int a=1 ; a<=5-i ; a++){
+                System.out.print("*");
+            }
+            for(int b=i ; b<=5 ; b++){
+                System.err.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
